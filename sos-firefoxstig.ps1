6e24de7d6583f45f1c839721f2889bf6a6810501
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-18 11:08:46 -0600

    Update sos-firefoxstig.ps1

--- sos-firefoxstig.ps1
+++ sos-firefoxstig.ps1
@@ -4,20 +4,19 @@
 $ErrorActionPreference= 'silentlycontinue'
 
 #Require elivation for script run
-Write-Output "Elevating priviledges for this process"
-do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
+#Requires -RunAsAdministrator
 
 $firefox64 = "C:\Program Files\Mozilla Firefox"
 $firefox32 = "C:\Program Files (x86)\Mozilla Firefox"
 Write-Output "Installing Firefox Configurations - Please Wait."
 Write-Output "Window will close after install is complete"
-If (Test-Path -Path $firefox64){
+If ((Test-Path -Path $firefox64) -eq $true){
     Copy-Item -Path .\Files\Config\* -Destination $firefox64 -Force -Recurse
     Write-Host "Firefox 64-Bit Configurations Installed"
 }Else {
     Write-Host "FireFox 64-Bit Is Not Installed"
 }
-If (Test-Path -Path $firefox32){
+If ((Test-Path -Path $firefox32) -eq $true){
     Copy-Item -Path .\Files\Config\* -Destination $firefox32 -Force -Recurse
     Write-Host "Firefox 32-Bit Configurations Installed"
 }Else {
