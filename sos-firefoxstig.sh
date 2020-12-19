579f37ccde9665e18d06b88bb4920ab0c6b3fd8c
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2020-12-19 05:37:23 +0000

    Update sos-firefoxstig.sh

--- sos-firefoxstig.sh
+++ sos-firefoxstig.sh
@@ -7,4 +7,8 @@ then
 	exit
 fi
 
-cp -R ./Files/Config/* /lib/firefox/
+mkdir -p /lib/firefox/browser/defaults/preferences/
+mkdir -p /lib/firefox/distribution/extensions/
+cp ./Files/browser/defaults/preferences/* /lib/firefox/browser/defaults/preferences/*
+cp ./Files/distribution/extensions/* /lib/firefox/distribution/extensions/*
+cp ./Files/browser/distribution/* /lib/firefox/distribution/*
