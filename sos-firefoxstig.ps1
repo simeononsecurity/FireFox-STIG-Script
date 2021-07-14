8027c899cc70740ab2ff5b946635f1f56210e42e
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2021-07-14 11:55:29 -0700

    Improve Script Output

--- sos-firefoxstig.ps1
+++ sos-firefoxstig.ps1
@@ -11,19 +11,19 @@ $ErrorActionPreference= 'silentlycontinue'
 $firefox64 = "C:\Program Files\Mozilla Firefox"
 $firefox32 = "C:\Program Files (x86)\Mozilla Firefox"
 
-Write-Output "Installing Firefox Configurations - Please Wait."
-Write-Output "Window will close after install is complete"
+Write-Host "Installing Firefox Configurations - Please Wait." -ForegroundColor White -BackgroundColor Black
+Write-Host "Window will close after install is complete" -ForegroundColor White -BackgroundColor Black
 If ((Test-Path -Path $firefox64) -eq $true){
     Copy-Item -Path .\Files\Config\* -Destination $firefox64 -Force -Recurse
-    Write-Host "Firefox 64-Bit Configurations Installed"
+    Write-Host "Firefox 64-Bit Configurations Installed" -ForegroundColor Green -BackgroundColor Black
 }Else {
-    Write-Host "FireFox 64-Bit Is Not Installed"
+    Write-Host "FireFox 64-Bit Is Not Installed" -ForegroundColor Red -BackgroundColor Black
 }
 If ((Test-Path -Path $firefox32) -eq $true){
     Copy-Item -Path .\Files\Config\* -Destination $firefox32 -Force -Recurse
-    Write-Host "Firefox 32-Bit Configurations Installed"
+    Write-Host "Firefox 32-Bit Configurations Installed" -ForegroundColor Green -BackgroundColor Black
 }Else {
-    Write-Host "FireFox 32-Bit Is Not Installed"
+    Write-Host "FireFox 32-Bit Is Not Installed" -ForegroundColor Red -BackgroundColor Black
 }
 
 .\Files\LGPO\LGPO.exe /g .\Files\GPO\
